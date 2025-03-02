53d3a6a0df0c15f92c57096932bfe4d79317c168
ramzanbhutto <kali.arch.78@gmail.com>
2025-03-02 20:54:55 +0500

    fixed

--- server.js
+++ server.js
@@ -3,22 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/todo-list', express.static(path.join(__dirname, 'todo-list')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/todo-list', (req, res) => {
-  res.sendFile(path.join(__dirname, 'todo-list', 'index.html'));
-});
-
-app.get('/python-app', (req, res) => {
-  res.send('This is the Python app. Implement routing as needed.');
-});
-
-app.get('/cpp-app', (req, res) => {
-  res.send('This is the C++ app. Implement routing as needed.');
 });
 
 app.listen(PORT, () => {
