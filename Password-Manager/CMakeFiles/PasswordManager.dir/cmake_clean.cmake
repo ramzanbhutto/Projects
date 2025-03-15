88d8e3e105ed0d347faa2509da3bcad1f61f284d
ramzanbhutto <kali.arch.78@gmail.com>
2025-03-16 00:35:21 +0500

    fixed index.html

---
+++ Password-Manager/CMakeFiles/PasswordManager.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PasswordManager.dir/link.d"
+  "CMakeFiles/PasswordManager.dir/src/encryption.cpp.o"
+  "CMakeFiles/PasswordManager.dir/src/encryption.cpp.o.d"
+  "CMakeFiles/PasswordManager.dir/src/gui.cpp.o"
+  "CMakeFiles/PasswordManager.dir/src/gui.cpp.o.d"
+  "CMakeFiles/PasswordManager.dir/src/main.cpp.o"
+  "CMakeFiles/PasswordManager.dir/src/main.cpp.o.d"
+  "PasswordManager"
+  "PasswordManager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PasswordManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Password-Manager/CMakeFiles/PasswordManager.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for PasswordManager.
+# This may be replaced when dependencies are built.
